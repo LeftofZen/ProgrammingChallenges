792623a9f5fe43b51e140edc260e69e2fa1a3946
Ben <bigbadbennybrother@gmail.com>
2020-10-23 00:47:59 +1100

    adds some brief comments explaining the sections of code

--- Edabit/TapCodeTranslation/Program.cs
+++ Edabit/TapCodeTranslation/Program.cs
@@ -25,12 +25,17 @@ namespace TapCodeTranslation
 
 			for (var i = 0; i < splits.Length / 2; i++)
 			{
+				// convert coordinates into ascii alphabet character number
 				var row = splits[i * 2].Length;
 				var column = splits[(i * 2) + 1].Length;
 				var letter = ((row - 1) * 5) + (column - 1) + 'a';
+
+				// special case for c/k
 				if (letter >= 'k')
 					letter++;
+
 				Console.WriteLine($"[{row}-{column}] => {letter - 'a'} => {letter} => {(char)letter} (from \"{splits[i * 2]}\"-\"{splits[(i * 2) + 1]}\")");
+
 				_ = sb.Append((char)letter);
 			}
 			var result = sb.ToString();
@@ -42,20 +47,30 @@ namespace TapCodeTranslation
 		{
 			Console.WriteLine($"Encoding \"{plaintext}\"");
 			var sb = new StringBuilder();
+
 			foreach (var c in plaintext)
 			{
+				// can't modify a loop variable
 				var cc = c;
+
+				// special case for c/k
 				if (cc == 'k')
-					cc -= (char)('k' - 'c');
-				if (cc > 'k')
+					cc = 'c';
+				if (cc >= 'k')
 					cc--;
+
+				// convert ascii character into coordinates
 				var num = cc - 'a';
 				var row = (num / 5) + 1;
 				var column = (num + 1) % 5;
+
+				// mod 5 will set column 5 to 0, but it needs to be 5
 				if (column == 0)
 					column = 5;
+
 				Console.WriteLine($"{(int)c - 'a'} => {num} => [{row}-{column}]");
 
+				// expand the number of does into a sequence of dots
 				_ = sb.Append('.', row);
 				_ = sb.Append(' ');
 				_ = sb.Append('.', column);
